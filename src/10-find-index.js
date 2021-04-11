ff253902137409e51811d1bf99631549e5519e30
Aliaksandr Torkhau <torkhau@gmail.com>
2021-04-11 20:31:21 +0300

    solved 10 task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 /**
  * Given a sorted array, find the index of the element with the given value.
  * Time complexity should be O(logN)
@@ -11,8 +12,18 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  if (array.length === 1) return 0;
+  if (array.length === 2) return (array[0] === value ? 0 : 1);
+  let i = Math.floor((array.length - 1) / 2);
+  while (!(array[i] === value)) {
+    if (array[i] < value) {
+      i = Math.floor((array.length - i) / 2) + i;
+    } else {
+      i = Math.floor((i - 1) / 2);
+    }
+  }
+  return i;
 }
 
 module.exports = findIndex;
